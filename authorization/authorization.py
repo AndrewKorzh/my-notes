616d606d0178528edcd738de0ed9e631402a7bfa
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2025-01-29 22:26:44 +0300

    auth

---
+++ authorization/authorization.py
@@ -0,0 +1,130 @@
+from fastapi import FastAPI, HTTPException, Request, Depends
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi.responses import JSONResponse
+
+from pydantic import BaseModel
+import jwt
+import datetime
+from typing import Optional
+import bcrypt
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+from db_handler import DBHandler
+
+DB_PATH = "user_auth.db"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = "SECRET_KEY"
+MICROSERVICE_SECRET_KEY = "MICROSERVICE_SECRET_KEY"
+
+
+class MicroserviceAuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        token = request.headers.get("authorization-microservice")
+        if token and token.startswith("Bearer "):
+            token = token.split(" ")[1]
+            try:
+                payload = jwt.decode(token, MICROSERVICE_SECRET_KEY, algorithms=[ALGORITHM])
+                request.state.service = payload
+            except jwt.ExpiredSignatureError:
+                return self._generate_error_response("Токен микросервиса истёк")
+            except jwt.InvalidTokenError:
+                return self._generate_error_response("Недействительный токен микросервиса")
+
+        else:
+            return self._generate_error_response("Отсутствует 'authorization-microservice' или начало не с 'Bearer '")
+        
+        return await call_next(request)
+
+    def _generate_error_response(self, error_detail: str):
+        return JSONResponse(
+            status_code=401,
+            content={"detail": error_detail}
+        )
+
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+class RegisterRequest(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Все источники
+    allow_credentials=True,
+    allow_methods=["*"],  # Все HTTP-методы (GET, POST и т. д.)
+    allow_headers=["*"],  # Все заголовки
+)
+app.add_middleware(MicroserviceAuthMiddleware)
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.PyJWTError:
+        return None
+    
+def create_access_token(data: dict, expires_delta: datetime.timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.datetime.now(datetime.UTC) + expires_delta
+    else:
+        expire = datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def hash_password(password: str) -> str:
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+@app.post("/login", response_model=Token)
+async def login(request: LoginRequest):
+    db = DBHandler(DB_PATH)
+    if not db.user_exists(request.username):
+        raise HTTPException(status_code=400, detail="Пользователь не существует")
+    expected_password_hash = db.get_password(request.username)
+    if not expected_password_hash:
+        raise HTTPException(status_code=400, detail="Пароль не найден")
+    if isinstance(expected_password_hash, bytes):
+        expected_password_hash = expected_password_hash.decode('utf-8')
+
+    verification = verify_password(request.password, expected_password_hash)
+    if not verification:
+        raise HTTPException(status_code=400, detail="Неверный пароль")
+    access_token = create_access_token(data={"sub": request.username})
+    db.close()
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.post("/register", response_model=Token)
+async def registration(request: RegisterRequest):
+    db = DBHandler(DB_PATH)
+    if db.user_exists(request.username):
+        raise HTTPException(status_code=400, detail="Такой пользователь существует")
+    
+    db.add_user(username=request.username, password_hash=hash_password(request.password))
+    access_token = create_access_token(data={"sub": request.username})
+    db.close()
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("authorization:app", host="0.0.0.0", port=8000, reload=True)
+
+
+
---
+++ authorization/db_handler.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+class DBHandler:
+    def __init__(self, db_path):
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
+        self._create_table()
+
+    def _create_table(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL
+        )
+        """)
+        self.conn.commit()
+
+    def add_user(self, username, password_hash):
+        try:
+            if self.user_exists(username):
+                return f"Пользователь с именем '{username}' уже существует."
+            self.cursor.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", 
+                                (username, password_hash))
+            self.conn.commit()
+
+            return True
+        except sqlite3.IntegrityError:
+            return f"Ошибка базы данных при добавлении пользователя '{username}'."
+        except Exception as e:
+            return f"Произошла ошибка при добавлении пользователя: {str(e)}"
+
+    def user_exists(self, username) -> bool:
+        self.cursor.execute("SELECT 1 FROM users WHERE username = ?", (username,))
+        return self.cursor.fetchone() is not None
+
+    def get_password(self, username) -> str | None:
+        self.cursor.execute("SELECT password_hash FROM users WHERE username = ?", (username,))
+        result = self.cursor.fetchone()
+        if result is None:
+            return None
+        return result[0]
+
+    def close(self):
+        self.conn.close()
+
+
+if __name__ == "__main__":
+    db_path = "user_auth.db"
+    hash_example = "hash-example"
+    username = "john_doe"
+
+    db = DBHandler(db_path=db_path)    
+    try_to_add = db.add_user(username, hash_example)
+    is_exists = db.user_exists("john_doe")
+    password = db.get_password("john_doe")
+
+    print(f"Пользователь {username} добавлен: {try_to_add}")
+    print(f"Пользователь {username} существует: {is_exists}")
+    print(f"Password: {password}")
+
+    db.close()
